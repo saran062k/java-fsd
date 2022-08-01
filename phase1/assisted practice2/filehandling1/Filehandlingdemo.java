49bafe759ed64525f713e10f46f75640d9abee04
saran062k <v.saranit06@gmail.com>
2022-08-01 12:31:45 +0530

    first commit

---
+++ phase1/assisted practice2/filehandling1/Filehandlingdemo.java
@@ -0,0 +1,36 @@
+package filehandling1;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Filehandlingdemo {
+
+
+	public static void main(String[] args) throws IOException {
+		File file = new File("D:\\filename.txt");
+		if(file.createNewFile()) {
+			System.out.println(file.exists());
+		}
+		
+		FileWriter fw = new FileWriter(file);
+		fw.write("Hello Everyone \nHow are you");
+		fw.close();
+		System.out.println("Reading content in the file");
+		Scanner reader = new Scanner(file);
+		while (reader.hasNext()) {
+			String content = reader.nextLine();
+			System.out.println(content);
+		}
+		System.out.println("File reading is over");
+		reader.close();
+		System.out.println("\n\nAppending...");
+		FileWriter fw1 = new FileWriter(file, true);
+		fw1.write("\nWelcome to java");
+		fw1.flush();
+		System.out.println("new text is appended into the file");
+
+	}
+
+
+}
